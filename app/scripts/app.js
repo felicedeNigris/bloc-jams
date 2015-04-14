5ce608c0c1fa5ad1803a79eb25a6323fa35f139a
Felice DeNigris <felideni@gmail.com>
2015-04-14 20:39:45 +0000

    Added a profile and subscription page with jquery tabs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,5 +4,4 @@ console.log("hello from app/scripts/app.js");
 require("./landing");
 require("./collection")
 require('./album');
-require("./collection");
-
+require("./profile");
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -291,9 +291,7 @@ console.log("hello from app/scripts/app.js");
 require("./landing");
 require("./collection")
 require('./album');
-require("./collection");
-
-
+require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -409,6 +407,33 @@ $( ".navbar-header" ).click(function() {
   
 });//end of $(document) function
 
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+ // holds the name of our tab button container for selection later in the function
+ var tabsContainer = ".user-profile-tabs-container"
+ var selectTabHandler = function(event) {
+ };
+
+  var tabsContainer = ".user-profile-tabs-container"
+ var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+   selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+
+  if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
