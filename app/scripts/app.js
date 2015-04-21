278b56b1985e7132ea0694c216a66e66503a913b
vagrant <vagrant@precise32.(none)>
2015-04-21 03:30:56 +0000

    converted bloc jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,20 @@
  //require('./collection');
  //require('./profile');
  
- angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+ blocJams = angular.module('BlocJams', ['ui.router']);
+ 
+ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+ }]);
+ 
+ // This is a cleaner way to call the controller than crowding it on the module definition.
+ blocJams.controller('Landing.controller', ['$scope', function($scope) {
     $scope.subText= "Turn the trebble up!";
 
     
@@ -23,6 +36,7 @@
 
 
 
+
    $scope.subTitleClicked = function(){
      shuffle($scope.albumURLs);
    }; 
@@ -35,7 +49,7 @@
      '/images/album-placeholders/album-4.jpg',
      '/images/album-placeholders/album-5.jpg',
      '/images/album-placeholders/album-6.jpg',  
-     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-7.jpg',  
      '/images/album-placeholders/album-8.jpg',
      '/images/album-placeholders/album-9.jpg',
    ];
