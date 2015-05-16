f426c076528b3764016a37ead45700a9bfc967dc
Felice DeNigris <felideni@gmail.com>
2015-05-16 16:56:21 -0400

    Added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -126,9 +126,16 @@
 
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
+
+  SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
+ 
 }]);
  
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     var currentSoundFile = null;
     var trackIndex = function(album, song) {
      return album.songs.indexOf(song);
@@ -176,6 +183,11 @@
          currentSoundFile.setTime(time);
        }
      },
+
+     onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
+
      setSong: function(album, song) {
       if (currentSoundFile) {
           currentSoundFile.stop();
@@ -183,15 +195,23 @@
 
       this.currentAlbum = album;
       this.currentSong = song;
+
       currentSoundFile = new buzz.sound(song.audioUrl, {
       formats: [ "mp3" ],
       preload: true
       });
- 
+
+       currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
+  
       this.play();
       }
-    };
- });
+    }
+}]);
+
+
+
 
    blocJams.directive('slider', ['$document', function($document){
 
@@ -289,3 +309,35 @@
     }
   };
 }]); // Make sure to close out the parentheses and brackets
+
+blocJams.filter('timecode', function(){ // filters timecode to seconds
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ })
+
+
+
