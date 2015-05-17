e1383e9bef660e3b082656a9aedd6752b1ff088f
Felice DeNigris <felideni@gmail.com>
2015-05-17 12:36:36 -0400

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -127,6 +127,16 @@
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+
+   $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+ 
+
   SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -145,6 +155,7 @@
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -184,6 +195,13 @@
        }
      },
 
+     setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+
      onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
@@ -200,6 +218,8 @@
       formats: [ "mp3" ],
       preload: true
       });
+
+      currentSoundFile.setVolume(this.volume);
 
        currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -414,6 +414,16 @@ console.log("this is album.js");
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+
+   $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+ 
+
   SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -432,6 +442,7 @@ console.log("this is album.js");
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -471,6 +482,13 @@ console.log("this is album.js");
        }
      },
 
+     setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+
      onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
@@ -487,6 +505,8 @@ console.log("this is album.js");
       formats: [ "mp3" ],
       preload: true
       });
+
+      currentSoundFile.setVolume(this.volume);
 
        currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
